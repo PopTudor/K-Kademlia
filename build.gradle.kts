1a2e48794d86d38434bdc4abcdaede9d379cdcd8
Tudor Pop <tudor14pop@gmail.com>
2018-12-08 00:40:34 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.0"
+}
+
+group = "Kademlia"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1")
+
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/com/kademlia/NodeId.kt
@@ -0,0 +1,6 @@
+package com.kademlia
+
+class NodeId(val publicKey: String = "", val ip: String, val port: String) {
+
+
+}
